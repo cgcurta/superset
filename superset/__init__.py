d704caa609adda61a5dd61e1e0c47a835e789342
Cal Curtain <calyph.curtain@harrison.ai>
2024-01-27 18:35:13 +1100

    Hashed global dependencies

--- superset/__init__.py
+++ superset/__init__.py
@@ -36,15 +36,15 @@ from superset.security import SupersetSecurityManager
 #  to declare "global" dependencies is to define it in extensions.py,
 #  then initialize it in app.create_app(). These fields will be removed
 #  in subsequent PRs as things are migrated towards the factory pattern
-app: Flask = current_app
-cache = cache_manager.cache
-conf = LocalProxy(lambda: current_app.config)
-get_feature_flags = feature_flag_manager.get_feature_flags
-get_manifest_files = manifest_processor.get_manifest_files
-is_feature_enabled = feature_flag_manager.is_feature_enabled
-results_backend = LocalProxy(lambda: results_backend_manager.results_backend)
-results_backend_use_msgpack = LocalProxy(
-    lambda: results_backend_manager.should_use_msgpack
-)
-data_cache = LocalProxy(lambda: cache_manager.data_cache)
-thumbnail_cache = LocalProxy(lambda: cache_manager.thumbnail_cache)
+# app: Flask = current_app
+# cache = cache_manager.cache
+# conf = LocalProxy(lambda: current_app.config)
+# get_feature_flags = feature_flag_manager.get_feature_flags
+# get_manifest_files = manifest_processor.get_manifest_files
+# is_feature_enabled = feature_flag_manager.is_feature_enabled
+# results_backend = LocalProxy(lambda: results_backend_manager.results_backend)
+# results_backend_use_msgpack = LocalProxy(
+#     lambda: results_backend_manager.should_use_msgpack
+# )
+# data_cache = LocalProxy(lambda: cache_manager.data_cache)
+# thumbnail_cache = LocalProxy(lambda: cache_manager.thumbnail_cache)
